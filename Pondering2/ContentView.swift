716dec2ed0a24112b2007f8749e4aa11a4a7503b
triforcepotato <prieshajhala@gmail.com>
2023-07-19 13:08:19 -0700

    check

--- Pondering2/ContentView.swift
+++ Pondering2/ContentView.swift
@@ -9,18 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+        
+        @State private var journalEntry = ""
+        
+        NavigationStack {
+            ZStack {
+                
+                Image ("Pond")
+                    .resizable()
+                    .ignoresSafeArea()
+                    .aspectRatio(contentMode: .fill)
+                
+                VStack (spacing : 20) {
+                    
+                    Spacer()
+                        
+                    
+                    Text("pick-me-ups!")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.green)
+                    
+                    TextField("what made you smile today?", text: $journalEntry)
+                        .multilineTextAlignment(.center)
+                        .border(Color.green, width: 2)
+                        .cornerRadius(5)
+                    
+                    Button("Submit!") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .tint(.teal)
+                                
+                    Spacer()
+                        .frame(height:550)
+                    
+                    NavigationLink(destination: journalEntries()) {
+                        Text("View Journal Entries")
+                    }
+                    .tint(.teal)
+                    
+                    
+                    Spacer()
+                    
+                }//vstack
+                .padding()
+                
+            }//zstack
+        } //navstack
+        
+    } //view
+} //struct
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+//hello
