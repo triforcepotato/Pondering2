2b9ec9285edc0182156b7f8234ed0123764987fa
triforcepotato <prieshajhala@gmail.com>
2023-07-19 13:05:27 -0700

    Initial Commit

---
+++ Pondering2/Pondering2App.swift
@@ -0,0 +1,17 @@
+//
+//  Pondering2App.swift
+//  Pondering2
+//
+//  Created by Scholar on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Pondering2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
