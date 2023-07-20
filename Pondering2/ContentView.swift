8cbeee19717ce6dde8a29f93728788dcf34c85e4
triforcepotato <prieshajhala@gmail.com>
2023-07-20 10:19:07 -0700

    its working!!

--- Pondering2/ContentView.swift
+++ Pondering2/ContentView.swift
@@ -11,51 +11,84 @@ struct ContentView: View {
     
     @State private var journalEntry = ""
     @State private var lily = ""
+    @State private var duck = ""
+    @State private var flower = ""
     
     var body: some View {
         
             ZStack {
                 
-                Image ("Pond")
-                    .resizable()
-                    .ignoresSafeArea()
-                    .aspectRatio(contentMode: .fill)
+                ZStack {
+                    Image ("Pond")
+                        .resizable()
+                        .ignoresSafeArea()
+                        .aspectRatio(contentMode: .fill)
+                    
+                    VStack {
+                        
+                        Spacer()
+                            .frame(height : 170)
+                        Image(lily)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 300, height: 75)
+                        
+                        Spacer()
+                            .frame (height : 50)
+                        
+                        
+                        HStack {
+                            
+                            Spacer()
+                                .frame(height : 30)
+                            Image(duck)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 300, height: 75)
+                        } //hstack
+                        
+                        Spacer()
+                            .frame(height : 90)
+                        HStack {
+                            Image (flower)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 300, height: 75)
+                            Spacer()
+                                .frame(height : 30)
+                        } //hstack2
+                        
+                    } //vstack
+                } //zstack
                 
                 VStack (spacing : 20) {
                     
                     Spacer()
+                        .frame(height : 30)
                         
                     
                     Text("pick-me-ups!")
                         .font(.title)
                         .fontWeight(.semibold)
-                        .foregroundColor(Color.green)
+                        .foregroundColor(Color(red: 149/255, green: 172/255, blue: 129/255))
                     
                     TextField("what made you smile today?", text: $journalEntry)
                         .multilineTextAlignment(.center)
-                        .border(Color.green, width: 2)
+                        .border((Color(red: 149/255, green: 172/255, blue: 129/255)), width: 2)
                         .cornerRadius(5)
                     
                     Button("Submit") {
                         lily = "lilyPad"
+                        duck = "duckArt"
+                        flower = "flower"
+                        journalEntry = "" 
                     }
                     .buttonStyle(.borderedProminent)
-                    .tint(.teal)
+                    .tint((Color(red: 149/255, green: 172/255, blue: 129/255)))
                                 
                     Spacer()
                         .frame(height:550)
                     
-                    Image(lily)
-                        
-                   
-                        .resizable(resizingMode: .stretch)
-                        .aspectRatio(contentMode: .fit)
-                        .padding(.bottom)
-                    
-                    Spacer()
-                        
-
-                                    
                     
                 }//vstack
                 .padding()
